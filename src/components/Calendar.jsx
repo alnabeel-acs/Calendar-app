5c2f38305ca88c9fc960359d74d4e0d50c244b45
alnabeel-acs <111437266+alnabeel-acs@users.noreply.github.com>
2024-03-13 16:23:02 +0530

    first

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,21 +1,30 @@
 import React from "react";
-import Fullcalendar from "@fullcalendar/react";
+import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
 function Calendar() {
+  const events = [
+    {
+      title: "Test Event",
+      start: "2024-03-13", // Should be in ISO 8601 format
+      end: "2024-03-13" // Should be in ISO 8601 format
+    }
+  ];
+
   return (
     <div>
-      <Fullcalendar
+      <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         initialView={"dayGridMonth"}
         headerToolbar={{
           start: "today prev,next", // will normally be on the left. if RTL, will be on the right
           center: "title",
-          end: "dayGridMonth,timeGridWeek,timeGridDay", // will normally be on the right. if RTL, will be on the left
+          end: "dayGridMonth,timeGridWeek,timeGridDay" // will normally be on the right. if RTL, will be on the left
         }}
         height={"90vh"}
+        events={events}
       />
     </div>
   );
